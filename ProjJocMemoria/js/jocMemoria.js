4adb6482880c8a0113cdcadff7a9fb953098ef12
jaume_aguilo <jaguilopiza@iessonferrer.net>
2022-10-24 17:57:41 +0200

    24/10/22 - Cambio la nombre de la clase Memoria por Joc, se desarrolla más el método colocarElementos.

--- ProjJocMemoria/js/jocMemoria.js
+++ ProjJocMemoria/js/jocMemoria.js
@@ -22,26 +22,26 @@ class Tablero {
 
         this.arrayTablero = [];
 
-        if ((this.filas + this.columnas) % 2 == 0) {
+        for (let fila = 0; fila < this.filas; fila++) {
+            this.arrayTablero[fila] = [];
 
-            for (let fila = 0; fila < this.filas; fila++) {
-                this.arrayTablero[fila] = [];
-
-                for (let columna = 0; columna < this.columnas; columna++) {
-                    this.arrayTablero[fila][columna] = '';
-                }
+            for (let columna = 0; columna < this.columnas; columna++) {
+                this.arrayTablero[fila][columna] = '';
             }
         }
     }
 
     pintarTablero() {
+
         document.write('<table>');
+
         for (let i = 0; i < this.filas; i++) {
             document.write('<tr>');
 
             for (let j = 0; j < this.columnas; j++) {
                 if (this.arrayTablero[i][j] != 0) {
                     document.write('<td>' + this.arrayTablero[i][j] + '</td>');
+
                 } else {
                     document.write('<td></td>');
                 }
@@ -52,36 +52,36 @@ class Tablero {
     }
 }
 
-class Memoria extends Tablero {
+class Joc extends Tablero{
 
-    constructor(filas, columnas) {
-        super(filas, columnas);
+    constructor(filas, columnas){
+        super(filas,columnas);
+
         this.colocarElementos();
     }
 
     colocarElementos() {
-        elementos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        casillasTablero = this.filas * this.columnas;
-        contadorCasillas = 0;
+
+        let elementos = ["🧡", "😈", "💚", "😳", "💜", "🤎", "🖤", "😊", "🤢", "💩"];
+        let casillasTablero = this.filas * this.columnas;
+        let contadorCasillas = 0;
 
         while (contadorCasillas < casillasTablero) {
-            posFila = Math.floor(Math.random() * this.filas);
-            posColumna = Math.floor(Math.random() * this.columnas);
+            let posFila = Math.floor(Math.random() * this.filas);
+            let posColumna = Math.floor(Math.random() * this.columnas);
 
-            for(i = 0; i < count(elementos); i++){
+            for(let i = 0; i < 2; i++){
 
-                if (this.arrayTablero[posFila][posColumna] == '') {
-                    this.arrayTablero[posFila][posColumna] = elementos[i];
-                    contadorCasillas++;
+                for(let j = 0; j < elementos.length; j++){
+                    
+                    this.arrayTablero[posFila][posColumna] = elementos[j];
+                    contadorCasillas++
                 }
-
-            }
-
-
+            }            
         }
     }
 }
 
-let memoria1 = new Memoria(numFilas, numColumnas);
-memoria1.pintarTablero();
-console.log(memoria1.arrayTablero);
+let joc1 = new Joc(numFilas, numColumnas);
+console.log(joc1.arrayTablero);
+joc1.pintarTablero();

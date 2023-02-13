d78fffe8d2cb10e093dd3a9f14daa9eac93ac356
jaume_aguilo <jaguilopiza@iessonferrer.net>
2023-02-13 17:46:30 +0100

    13/02/23 - JSON Y AJAX.

--- ProyValidacionDNI/ajax/city.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function obtain(){
-        $conexion = mysqli_connect('localhost', 'root','1234');
-        mysqli_select_db($conexion, 'world');
-        $consulta = mysqli_prepare($conexion, "SELECT * FROM city;");
-    }
---
+++ ProyValidacionDNI/ajax/php/city.php
@@ -0,0 +1,33 @@
+<?php
+ini_set('html_errors', 1);
+ini_set('display_errors', 1);
+
+$conexion = mysqli_connect('localhost', 'root', '1234');
+mysqli_select_db($conexion, 'world');
+$consulta = mysqli_prepare($conexion, "SELECT city.Name FROM city;");
+$consulta->execute();
+$result = $consulta->get_result();
+
+$arrayNombres = array();
+while ($myrow = $result->fetch_assoc()) {
+    array_push($arrayNombres, $myrow);
+}
+
+$nombre = $_REQUEST["nombre"];
+$pista = "";
+
+if ($nombre !== "") {
+    $nombre = strtolower($nombre);
+    $long = strlen($nombre);
+    foreach ($arrayNombres as $a) {
+        if (stristr($nombre, substr($a['Name'], 0, $long))) {
+            if ($pista === "") {
+                $pista = $a['Name'];
+            } else {
+                $pista .= "," . $a['Name'];
+            }
+        }
+    }
+}
+
+echo $pista === "" ? "No hay sugerencias encontradas." : $pista;
